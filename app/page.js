65c68e6adb67aea372bf0c13a25594a26cff60ee
jomno <jake89577@gmail.com>
2024-06-19 23:54:28 +0900

    카카오 로그인 기능 완료

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import KakaoButton from "./_components/KakaoButton";
 
 export default function Home() {
   return (
@@ -7,13 +8,7 @@ export default function Home() {
       <div className="font-[600] text-[30px] mt-[43px]">
         Family Diary와 함께 <br /> 추억을 만들어 볼까요?
       </div>
-      <button
-        type="button"
-        className="mt-[69px] w-full bg-[#FDE500] flex items-center justify-center py-[18px] rounded-[26px]"
-      >
-        <Image src="/kakao.svg" width={16} height={15} alt="카카오로그인" />
-        <div className="text-sm font-[600] ml-[15px]">카카오로 시작하기</div>
-      </button>
+      <KakaoButton />
     </div>
   );
 }
