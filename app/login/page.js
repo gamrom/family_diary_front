3b373acb3f4126ec8bf2f4feb20e6b5c4181ff51
jomno <jake89577@gmail.com>
2024-06-20 00:13:16 +0900

    임시 저장

---
+++ app/login/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import KakaoButton from "@/app/_components/KakaoButton";
+
+export default function Home() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <Image src="/circle_char.svg" width={89} height={89} alt="메인캐릭터" />
+      <div className="font-[600] text-[30px] mt-[43px]">
+        Family Diary와 함께 <br /> 추억을 만들어 볼까요?
+      </div>
+      <KakaoButton />
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,14 +1,23 @@
-import Image from "next/image";
-import KakaoButton from "./_components/KakaoButton";
+import { fetcher } from "@/utils/fetcher";
+import { cookies } from "next/headers";
 
-export default function Home() {
-  return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <Image src="/circle_char.svg" width={89} height={89} alt="메인캐릭터" />
-      <div className="font-[600] text-[30px] mt-[43px]">
-        Family Diary와 함께 <br /> 추억을 만들어 볼까요?
-      </div>
-      <KakaoButton />
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+async function getCurrentUser() {
+  const res = await fetcher(`/current_user`, cookies());
+
+  if (!res.ok) {
+    return null;
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  return <>로그인 완료</>
 }
