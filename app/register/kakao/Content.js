9e0e48ea774d7bce8f86ff0a828a4d0ece49f1be
김은식 <gamrom01@naver.com>
2024-06-22 03:10:33 +0900

    suspense해결

--- app/register/kakao/Content.js
+++ app/register/kakao/Content.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useRouter } from "next/navigation";
 import { useSearchParams } from "next/navigation";
 import { LoadingFallback } from "@/app/_components/LoadingFallback";
@@ -12,25 +12,29 @@ const KakaoLoginContent = () => {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
 
-  if (typeof window !== "undefined") {
-    postKakaoAuth({
-      code,
-      redirect_uri: `${window?.location?.origin}/register/kakao`,
+  // if (typeof window !== "undefined") {
+  postKakaoAuth({
+    code,
+    redirect_uri: `${window?.location?.origin}/register/kakao`,
+  })
+    .then((res) => {
+      if (res.status === 200) {
+        router.push("/?login=true");
+      }
     })
-      .then((res) => {
-        if (res.status === 200) {
-          router.push("/?login=true");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("카카오 로그인에 실패했습니다.");
-      });
-  }
+    .catch((error) => {
+      console.log(error);
+      alert("카카오 로그인에 실패했습니다.");
+    });
+  // }
 
   return <LoadingFallback />;
 };
 
 export const Content = () => {
-  return <KakaoLoginContent />;
+  return (
+    <Suspense fallback={<LoadingFallback />}>
+      <KakaoLoginContent />
+    </Suspense>
+  );
 };
