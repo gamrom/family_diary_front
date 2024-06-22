40728f8bf322ec9fe9ea31c816a96967fe43e0a9
wonno <jake89577@gmail.com>
2024-06-23 01:43:26 +0900

    minor fix

---
+++ app/making_pdf/[id]/page.js
@@ -0,0 +1,19 @@
+// import { Content } from "./Content";
+import { fetcher } from "@/app/_hooks/fetcher";
+import { cookies } from "next/headers";
+
+async function getDiary(id) {
+  const res = await fetcher(`/diaries/${id}`, cookies());
+
+  if (!res.ok) {
+    return null;
+  }
+  return res.json();
+}
+
+export default async function Page({ params }) {
+  const { id } = params;
+
+  const diary = await getDiary(id);
+  // return <Content diary={diary} />;
+}
