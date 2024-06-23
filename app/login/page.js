fc0baa72741162c2bcaba42822f6ac6ca53d6159
김은식 <gamrom01@naver.com>
2024-06-24 02:03:00 +0900

    login디자인수정

--- app/login/page.js
+++ app/login/page.js
@@ -5,11 +5,16 @@ import { ScreenCenterLayout } from "../_components/ScreenCenterLayout";
 
 export default function Home() {
   return (
-    // <div className="h-screen flex flex-col items-center justify-center circle-btn-shadow main-container">
     <ScreenCenterLayout>
       <div className="flex flex-col items-center">
-        <Image src="/circle_char.svg" width={89} height={89} alt="메인캐릭터" />
-        <div className="font-[600] text-[30px] mt-[43px]">
+        <Image
+          src="/circle_char.svg"
+          width={89}
+          height={89}
+          alt="메인캐릭터"
+          className="circle-btn-shadow"
+        />
+        <div className="font-[600] text-[30px] mt-[43px] text-center">
           Family Diary와 함께 <br /> 추억을 만들어 볼까요?
         </div>
         <KakaoButton />
