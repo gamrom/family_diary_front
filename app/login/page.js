b129fc12bca69834cae5ae5ee57df6ec1f04e344
김은식 <gamrom01@naver.com>
2024-06-23 14:50:31 +0900

    login page

--- app/login/page.js
+++ app/login/page.js
@@ -1,15 +1,19 @@
 import Image from "next/image";
 
 import KakaoButton from "@/app/_components/KakaoButton";
+import { ScreenCenterLayout } from "../_components/ScreenCenterLayout";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center circle-btn-shadow main-container">
-      <Image src="/circle_char.svg" width={89} height={89} alt="메인캐릭터" />
-      <div className="font-[600] text-[30px] mt-[43px]">
-        Family Diary와 함께 <br /> 추억을 만들어 볼까요?
+    // <div className="h-screen flex flex-col items-center justify-center circle-btn-shadow main-container">
+    <ScreenCenterLayout>
+      <div className="flex flex-col items-center">
+        <Image src="/circle_char.svg" width={89} height={89} alt="메인캐릭터" />
+        <div className="font-[600] text-[30px] mt-[43px]">
+          Family Diary와 함께 <br /> 추억을 만들어 볼까요?
+        </div>
+        <KakaoButton />
       </div>
-      <KakaoButton />
-    </div>
+    </ScreenCenterLayout>
   );
 }
