fd1be68a9ffd9ec5a42f5a1d9b3e4eb112dbdb59
jomno <jake89577@gmail.com>
2024-06-25 11:59:19 +0900

    set puppeteer option changed5

--- app/api/make-pdf/route.js
+++ app/api/make-pdf/route.js
@@ -30,16 +30,16 @@ export async function GET(request) {
         args: [
           "--no-sandbox",
           "--disable-setuid-sandbox",
-          "--disable-dev-shm-usage",
-          "--window-size=1920,1080",
-          '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"',
+          // "--disable-dev-shm-usage",
+          // "--window-size=1920,1080",
+          // '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"',
         ],
         ignoreDefaultArgs: ["--disable-extensions"],
         headless: true,
         timeout: 60000,
-        env: {
-          DISPLAY: ":99.0",
-        },
+        // env: {
+        //   DISPLAY: ":99.0",
+        // },
       });
     }
     const page = await browser.newPage();
