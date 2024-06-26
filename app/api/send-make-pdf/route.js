6043e3da37e20d899daae520949be4b3c632fe8b
jomno <jake89577@gmail.com>
2024-06-27 02:48:17 +0900

    send-make-pdf로 변환

---
+++ app/api/send-make-pdf/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const url = searchParams.get("url");
+
+  const response = await fetch("http://172.30.1.60:3000/api/make-pdf?url=" + url);
+
+  if (!response.ok) {
+    return NextResponse.error();
+  }
+
+  try {
+    return new Response(response.body);
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Failed to generate PDF" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
